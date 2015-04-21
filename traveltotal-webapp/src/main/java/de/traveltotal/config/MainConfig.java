e81a96dae5ca0368a31bb1098d60b678aa36af2d
mherttrich <mherttrich@users.noreply.github.com>
2015-04-21 18:31:03 +0200

    just written from github editor, might not work ;)

--- traveltotal-webapp/src/main/java/de/traveltotal/config/MainConfig.java
+++ traveltotal-webapp/src/main/java/de/traveltotal/config/MainConfig.java
@@ -3,6 +3,9 @@ package de.traveltotal.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.*;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
 @ComponentScan(basePackages = {"de.traveltotal"},
@@ -11,5 +14,18 @@ import org.springframework.context.annotation.*;
 //@EnableTransactionManagement
 @PropertySource("${props.path:classpath:}/application.properties")
 @Import({DatabaseConfig.class})
-public class MainConfig {
+public class MainConfig implements ResourceLoaderAware {
+     private static final String USER_TEST_DATA_FILE_PATH = "classpath:user-test-data.txt";
+     
+    @Bean
+    public Resource userTestDataResource() {
+        return resourceLoader.getResource(USER_TEST_DATA_FILE_PATH);
+    }
+    
+        
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
 }
